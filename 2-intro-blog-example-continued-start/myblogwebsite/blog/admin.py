af2c419ebce6ec92a2b3e27e866aec23473f245d
Daniel Mouris <dgmouris@gmail.com>
2026-01-12 11:44:20 -0700

    class progress pushed

--- 2-intro-blog-example-continued-start/myblogwebsite/blog/admin.py
+++ 2-intro-blog-example-continued-start/myblogwebsite/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # we're going import our models in this file.
+from .models import Post
+
 # and "register" them on the admin so we can
 # browse them.
-
+admin.site.register(Post)
--- 2-intro-blog-example-continued-start/myblogwebsite/blog/models.py
+++ 2-intro-blog-example-continued-start/myblogwebsite/blog/models.py
@@ -1,4 +1,30 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-## this is going to be where you create the models
+'''
+To apply this model to the database.
 
+We need to run two commands
+1. python manage.py makemigrations
+    - This is going to generate a code file that if ran will
+    apply the table to the database.
+2. python manage.py migrate
+    - This is going to run the code file(s) above
+
+
+'''
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
