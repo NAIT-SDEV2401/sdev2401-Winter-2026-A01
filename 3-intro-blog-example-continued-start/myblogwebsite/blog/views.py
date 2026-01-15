b6891b6aa3e55f43717b5ea47ac1ab3aa7b42ef4
Daniel Mouris <dgmouris@gmail.com>
2026-01-15 11:18:14 -0700

    class progress pushed

--- 3-intro-blog-example-continued-start/myblogwebsite/blog/views.py
+++ 3-intro-blog-example-continued-start/myblogwebsite/blog/views.py
@@ -1,3 +1,12 @@
+# render is a special django function
+# that will return an html page with
+# some data inside (if you want)
 from django.shortcuts import render
 
-# Create your views here.
+# is going to be the controllers
+# of your app (think mvc architecture)
+# this is going to handle requests
+# return responses
+
+def post_list(request):
+    # request we can see more of this later
