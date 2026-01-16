607e28ed4364d1e547ddcf6caad887c9df3cf2b5
Daniel Mouris <dgmouris@gmail.com>
2026-01-16 13:45:15 -0700

    class progress pushed

--- 3-intro-blog-example-continued-start/myblogwebsite/blog/views.py
+++ 3-intro-blog-example-continued-start/myblogwebsite/blog/views.py
@@ -2,35 +2,35 @@
 # that will return an html page with
 # some data inside (if you want)
 from django.shortcuts import render
+# import the model
+from .models import Post
 
 # is going to be the controllers
 # of your app (think mvc architecture)
 # this is going to handle requests
 # return responses
-
 def post_list(request):
     # request we can see more of this later
     # what the user sent we have access to
     # a whole bunch here.
 
-    # here in this part of the view we will
-    # interact with our ORM.
-
+    # let's get the posts
+    # below is like (select * from post)
+    posts = Post.objects.all()
     # I want you to put a breakpoint here
-    breakpoint()
+    # breakpoint()
     # handy breakpoint commands
     # l or ll show where you are
     # c continue
     # n next
 
-
     # return a response to this request
     return render(
         request,
-        'blog/post_list.html'
-        # the above is looking in the
-        # templates folder that will be
-        # aggregated on load
+        'blog/post_list.html',
+        { "posts": posts }
+        # the above is sending context
+        # which is data to the template
     )
 
 
