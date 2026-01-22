f44aaa9eeebf080152f2c783bac4e577d0bf98d7
Daniel Mouris <dgmouris@gmail.com>
2026-01-22 10:55:11 -0700

    class progress pushed

--- 5-urls-and-views-start/urls_views_fundamentals/urls_views_fundamentals/urls.py
+++ 5-urls-and-views-start/urls_views_fundamentals/urls_views_fundamentals/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # we're going to include the app level urls
     path("", include("pet_adoption.urls")),
+    # pet_adoption.urls above is looking for
+    # the urls file inside of the pet_adoption
+    # app (needs to be added to INSTALLED_APPS in settings)
 ]
