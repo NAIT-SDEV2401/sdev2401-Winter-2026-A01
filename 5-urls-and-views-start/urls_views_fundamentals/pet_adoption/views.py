4310b8a227e3b0a266b06b20ee8b2632867aec19
Daniel Mouris <dgmouris@gmail.com>
2026-01-22 11:22:29 -0700

    class progress pushed

--- 5-urls-and-views-start/urls_views_fundamentals/pet_adoption/views.py
+++ 5-urls-and-views-start/urls_views_fundamentals/pet_adoption/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
+# over here we have a dictionary of values.
+PET_TYPES = {
+    'dog': {
+        'name': 'Dog',
+        'traits': 'Loyal, energetic, needs space and exercise.',
+        'lifestyle_fit': 'active'
+    },
+    'cat': {
+        'name': 'Cat',
+        'traits': 'Independent, cuddly, low-maintenance.',
+        'lifestyle_fit': 'quiet'
+    },
+    'rabbit': {
+        'name': 'Rabbit',
+        'traits': 'Gentle, small, requires calm environment.',
+        'lifestyle_fit': 'quiet'
+    },
+    'parrot': {
+        'name': 'Parrot',
+        'traits': 'Social, intelligent, needs stimulation.',
+        'lifestyle_fit': 'social'
+    }
+}
+
 # this is going to take in a request
 # and return a response.
 def home_page(request):
@@ -13,7 +37,7 @@ def home_page(request):
         "pet_adoption/home_page.html",
         # the above is the template
         # note don't include the "templates"
-        { "name", name }
+        { "name": name }
         # this line above passes the data
         # as context to the template
     )
