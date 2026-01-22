cbec85a0705220a89e78bd2605920f00c473184b
Daniel Mouris <dgmouris@gmail.com>
2026-01-22 11:18:03 -0700

    class progress pushed

--- 5-urls-and-views-start/urls_views_fundamentals/pet_adoption/views.py
+++ 5-urls-and-views-start/urls_views_fundamentals/pet_adoption/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # this is going to take in a request
 # and return a response.
 def home_page(request):
+    # we're going to create a variablle
+    name = "Humane Society"
 
     # we're going to return the response
     # which will be the template for us.
@@ -11,4 +13,7 @@ def home_page(request):
         "pet_adoption/home_page.html",
         # the above is the template
         # note don't include the "templates"
+        { "name", name }
+        # this line above passes the data
+        # as context to the template
     )
