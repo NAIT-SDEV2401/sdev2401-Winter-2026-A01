0e27635ef91a3875c3adbfebdaa3613cc8fe3fee
Daniel Mouris <dgmouris@gmail.com>
2026-01-22 10:50:23 -0700

    class progress pushed

--- 5-urls-and-views-start/urls_views_fundamentals/urls_views_fundamentals/urls.py
+++ 5-urls-and-views-start/urls_views_fundamentals/urls_views_fundamentals/urls.py
@@ -1,23 +1,10 @@
-"""
-URL configuration for urls_views_fundamentals project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path
+# where we're going to use include for the app
+from django.urls import path, include
 
+# this project level urls
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # we're going to include the app level urls
+    path("", include("pet_adoption.urls")),
 ]
