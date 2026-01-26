43aef639d9edf43780741333e2196532abd61702
Daniel Mouris <dgmouris@gmail.com>
2026-01-26 11:24:56 -0700

    class progress pushed

--- 7-orm-fundamentals-start/mysoftwarecompany/clients/admin.py
+++ 7-orm-fundamentals-start/mysoftwarecompany/clients/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# import the model
+from .models import Company
+
+# this is a file that you can add your models
+# to the admin sites.
+admin.site.register(Company)
+
+# for the sake of this course this is 99% of what we're
+# we're goign to use for the admin, but you can
+# customize it and there's a lot of customizations out ther.
--- 7-orm-fundamentals-start/mysoftwarecompany/clients/models.py
+++ 7-orm-fundamentals-start/mysoftwarecompany/clients/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+# Important note you don't have to care how the migrations
+# work, all you need to know is that it's performing
+# create table and alter table commands.
+
 # classes here that inherit from models.Model are going
 # to be what's in the database.
 class Company(models.Model):
@@ -12,6 +16,9 @@ class Company(models.Model):
     # this is going to make a field in the db called
     # email with max 100 chars in size.
     # this is also unique.
+
+    # Note: no need to add the id, it's added by default.
+
 
     # let's override __str__ so it's a bit nicer
     # when we print or call this.
