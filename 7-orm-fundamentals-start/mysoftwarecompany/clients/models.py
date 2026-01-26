b5dba153e50d17fcc86d9e7337032395b3b0e917
Daniel Mouris <dgmouris@gmail.com>
2026-01-26 11:05:30 -0700

    class progress pushed

--- 7-orm-fundamentals-start/mysoftwarecompany/clients/models.py
+++ 7-orm-fundamentals-start/mysoftwarecompany/clients/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# classes here that inherit from models.Model are going
+# to be what's in the database.
+class Company(models.Model):
+    # these below are going to be the fields in the
+    # database.
+    name = models.CharField(max_length=100)
+    # this is going to make a field in the db called
+    # name with max 100 chars in size.
+    email = models.EmailField(max_length=100, unique=True)
+    # this is going to make a field in the db called
+    # email with max 100 chars in size.
+    # this is also unique.
+
+    # let's override __str__ so it's a bit nicer
+    # when we print or call this.
+    def __str__(self):
+        return self.name
