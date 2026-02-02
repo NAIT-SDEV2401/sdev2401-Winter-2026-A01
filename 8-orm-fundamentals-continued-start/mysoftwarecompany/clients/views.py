ec56d7f63d713c813710e22e4a90ec44e88ec860
Daniel Mouris <dgmouris@gmail.com>
2026-02-02 10:23:20 -0700

    class progress pushed

--- 8-orm-fundamentals-continued-start/mysoftwarecompany/clients/views.py
+++ 8-orm-fundamentals-continued-start/mysoftwarecompany/clients/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+# import companies
+
+def list_companies(request):
+
+    # I want you to get all of the companies
+
+    return render(
+        # request
+        # template name "clients/list.html"
+        # pass what's from the database to the context.
+    )
