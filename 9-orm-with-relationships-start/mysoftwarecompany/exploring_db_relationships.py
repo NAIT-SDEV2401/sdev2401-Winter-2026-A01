e2bf06d5ae1f1977447b68c3d8dcf5bece63e6c6
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 10:46:35 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships.py
@@ -8,3 +8,10 @@ print("Django environment set up successfully.")
 # let's import the tables
 from clients.models import Employee, Company
 
+COMPANY_NAME = "Acme Inc."
+# get an instance
+acme_company = Company.objects.get(
+    name=COMPANY_NAME
+)
+
+breakpoint()
