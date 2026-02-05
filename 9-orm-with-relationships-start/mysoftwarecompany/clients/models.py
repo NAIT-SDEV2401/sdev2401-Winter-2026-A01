ac9d17a87a69b92a60789c24b85c85026c76802a
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 10:25:47 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/clients/models.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/clients/models.py
@@ -14,13 +14,14 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+class Employee(models.Model):
+    # core fields
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(
+        max_length=100, unique=True)
 
-'''
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set the field to now when the object is first created
+    updated_at = models.DateTimeField(auto_now=True)
 
-    # date fields
-    # the auto_now_add Automatically set the date when the record is created
-    date_joined = models.DateField(auto_now_add=True)
-    # the
-    updated_at = models.DateField(auto_now=True)
-
-'''
+    # create my foreign key relationship.
