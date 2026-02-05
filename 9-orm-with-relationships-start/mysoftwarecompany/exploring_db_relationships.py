e9bca92c71cc6dd6899200698b6320e97fe7d1d6
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 11:02:54 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships.py
@@ -14,4 +14,14 @@ acme_company = Company.objects.get(
     name=COMPANY_NAME
 )
 
-breakpoint()
+# let's get the employees from the acme_company instance
+all_employees = acme_company.employees.all()
+
+# another way to get all employees
+all_employees = Employee.objects.filter(
+    company=acme_company # instance
+)
+# both of these ways do the same thing
+print("All employees")
+print(all_employees)
+
