d52123929f22332c2147b009e4f6fc1d409a900e
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 11:30:56 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_2.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_2.py
@@ -4,3 +4,16 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysoftwarecompany.settings")
 django.setup()
 
 print("Django environment set up successfully.")
+
+# import the Role model.
+
+roles_data = [
+    {"name": "CEO", "description": "Chief Executive Officer"},
+    {"name": "Manager", "description": "Manages a team of employees"},
+    {"name": "Developer", "description": "Writes code and develops software"}
+]
+
+# I want you to loop through the roles
+# use get or create wit hthe data to create
+# roles.
+# display if it was created or selected from the db
