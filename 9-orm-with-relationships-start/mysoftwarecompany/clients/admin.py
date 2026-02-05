1251cc0b4035863eb2f3258fd237c999380428a3
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 10:39:36 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/clients/admin.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/clients/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Company
+from .models import (
+    Company,
+    Employee
+)
 
 # this is going to add it to the admin interface.
 admin.site.register(Company)
+admin.site.register(Employee)
