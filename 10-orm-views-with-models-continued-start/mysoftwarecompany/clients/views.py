969e16dbb0112075da737cad08f4e621e21ea8f2
Daniel Mouris <dgmouris@gmail.com>
2026-02-09 11:33:50 -0700

    class progress pushed

--- 10-orm-views-with-models-continued-start/mysoftwarecompany/clients/views.py
+++ 10-orm-views-with-models-continued-start/mysoftwarecompany/clients/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from .models import Company
+from .models import Company, Employee
 
 
 def list_companies(request):
@@ -34,11 +34,27 @@ def employees_search_results(request, company_id):
     # select the specific company.
     company = get_object_or_404(Company, id=company_id)
     # let's get a query parameter from the request object.
-    query = request.GET.get('q', '')
+    query = request.GET.get('q') # the value or None
 
-    breakpoint()
+    # check if the query exists
+    employees = None
+    if query:
+        # let's first search by the first name using icontains
+        # field look up docs here: https://docs.djangoproject.com/en/5.2/ref/models/querysets/#field-lookups
+        # remember that company.employees is queryset (many rows of instances)
+        # that we can filter the first name if it contains any part of the query.
+        employees = company.employees.filter(
+            first_name__icontains=query
+        )
+        # in sql
+        # SELECT * FROM clients_employees WHERE first_name LIKE '%query%'.
+    else: # there's no query
+        # if there's no query you can just provide an empty queryset
+        employees = Employee.objects.none()
+
 
     return render(
         request,
-        'clients/employees_search_results.html'
+        'clients/employees_search_results.html',
+        {'company': company, 'employees': employees, 'query': query }
     )
