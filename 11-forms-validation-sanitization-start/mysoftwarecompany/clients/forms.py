b0ee934d66d569101c27f20c3ccdffffb4472470
Daniel Mouris <dgmouris@gmail.com>
2026-02-12 10:47:00 -0700

    class progress pushed

--- 11-forms-validation-sanitization-start/mysoftwarecompany/clients/forms.py
+++ 11-forms-validation-sanitization-start/mysoftwarecompany/clients/forms.py
@@ -1,6 +1,8 @@
 # from django here we're going to import the forms module.
 from django import forms
 
+# docs for form fields here: https://docs.djangoproject.com/en/5.2/ref/forms/fields
+
 # this going to be what we use to validate our users data.
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, required=True)
