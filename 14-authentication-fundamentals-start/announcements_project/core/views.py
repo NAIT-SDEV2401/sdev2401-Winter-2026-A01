1192c99cd20ea35ebc554a85676c3c1361578a05
Daniel Mouris <dgmouris@gmail.com>
2026-03-05 10:39:52 -0700

    class progress pushed

--- 14-authentication-fundamentals-start/announcements_project/core/views.py
+++ 14-authentication-fundamentals-start/announcements_project/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+# we are using these two functions to authenticate a user.
 from django.contrib.auth import login, authenticate
 
 # import AuthenticationForm built in django.
@@ -7,7 +8,33 @@ from django.contrib.auth.forms import AuthenticationForm
 # import our new form here
 from .forms import UserRegistrationForm
 
+def custom_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        # check if valid.
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            # perform the check to see if the user exists
+            # with authenticate.
+            user = authenticate(request,
+                                username=username,
+                                password=password)
+            # returns a user if they exist or none.
+            # check if the user existists we're going login
+            # as the user and redirect them to the announcements
+            # page.
+            if user is not None:
+                login(request, user)
+                return redirect('announcement_list')
 
+    else:
+        form = AuthenticationForm()
+    return render(
+        request,
+        "core/login.html",
+        { "form": form }
+    )
 
 
 # let's create the view to register a user
