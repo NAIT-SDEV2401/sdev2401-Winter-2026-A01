77b9a466a9ef6c6d58737d9880cf30bc5e39c1e8
Daniel Mouris <dgmouris@gmail.com>
2026-03-05 10:41:21 -0700

    class progress pushed

--- 14-authentication-fundamentals-start/announcements_project/core/urls.py
+++ 14-authentication-fundamentals-start/announcements_project/core/urls.py
@@ -2,16 +2,14 @@ from django.urls import path
 # let's import the default LoginView
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import register
+from .views import register, custom_login
 
 
 urlpatterns = [
     path('register/', register, name="register"),
     # the login view is class based
     # more on this later
-    path("login/", LoginView.as_view(
-        template_name="core/login.html"
-    ), name="login"),
+    path("login/", custom_login, name="login"),
     # we're also going to add the logout view which is a form.
     path("logout/", LogoutView.as_view(), name="logout")
 ]
