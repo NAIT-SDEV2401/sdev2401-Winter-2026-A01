7b290b67f5eabc146f3e9cac1c4d709f1b10fec0
Daniel Mouris <dgmouris@gmail.com>
2026-03-05 11:13:17 -0700

    class progress pushed

--- 14-authentication-fundamentals-start/announcements_project/announcements/views.py
+++ 14-authentication-fundamentals-start/announcements_project/announcements/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 
+# let's import the login_required decorator.
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 from .models import Announcement
 
 # everyone can see if they're logged in.
+@login_required
 def announcement_list(request):
     announcements = Announcement.objects.all().order_by('-created_at')
     return render(
