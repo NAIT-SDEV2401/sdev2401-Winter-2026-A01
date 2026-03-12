d921cc48508d7b177a13ec741373a66d4f8b31e9
Daniel Mouris <dgmouris@gmail.com>
2026-03-12 11:45:01 -0600

    class progress pushed

--- 17-uploading-non-image-files-start/announcements_project/courses/views.py
+++ 17-uploading-non-image-files-start/announcements_project/courses/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import BulkAssignmentUploadForm
+
+
+# make the bulk_assignment_upload view
+# using the form we've created.
+@login_required
+def bulk_assignment_upload(request):
+    if request.method == "POST":
+        form = BulkAssignmentUploadForm(
+            request.POST,
+            request.FILES,
+        )
+        if form.is_valid():
+            csv_file = form.cleaned_data.get("csv_file")
+
+            # below we're going to handle the parsing of
+            # the data.
+
+    else:
+
+        form = BulkAssignmentUploadForm()
+    return render(
+        request,
+        "courses/bulk_assignment_upload.html",
+        {"form": form},
+    )
