41f14d9be5e24354f0a43299ddf0ec5519e9cd1b
Daniel Mouris <dgmouris@gmail.com>
2026-03-16 10:42:01 -0600

    class progress pushed

---
+++ 18-custom-management-commands-start/announcements_project/courses/management/commands/import_courses.py
@@ -0,0 +1,15 @@
+# this is going to import all of our courses.
+# this is going to read a csv
+# and create course instances
+import csv
+
+import django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    # this will be the help text if a user doesn't know how
+    # what this command does.
+    help = "Import courses from a CSV file"
+
+    # we might be uploading various courses
+    # add an argument for the path of the csv file.
