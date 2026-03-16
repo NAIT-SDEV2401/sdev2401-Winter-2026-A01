112c5afc56c76d58c71c2a12d55a88f770f39e58
Daniel Mouris <dgmouris@gmail.com>
2026-03-16 11:26:26 -0600

    class progress pushed

---
+++ 18-custom-management-commands-start/announcements_project/courses/management/commands/export_courses.py
@@ -0,0 +1,45 @@
+import csv
+from django.core.management.base import BaseCommand
+
+from courses.models import Course
+
+
+# create management command
+class Command(BaseCommand):
+    help = "Exports courses to a csv file"
+
+    # that takes output_file as an argument
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "output_file", type=str, help="Path to output csv course file"
+        )
+
+    def handle(self, *args, **kwargs):
+        output_file = kwargs.get("output_file")
+
+        # errors if it doesn't have it.
+        if not output_file:
+            self.stdout.write(
+                self.style.ERROR(
+                    "Please provide an output file",
+                )
+            )
+
+        # we're going to get all of the courses
+        courses = Course.objects.all()
+        # I want you to create a csv with all of the courses
+        with open(output_file, mode="w", newline="") as file:
+            writer = csv.writer(file)
+            # add the header line
+            writer.writerow(["course title", "course description"])
+            # loop through courses and add them to the csv.
+            for course in courses:
+                writer.writerow([course.title, course.description])
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully exported {courses.count} to {output_file}"
+                )
+            )
+
+        # name and description.
