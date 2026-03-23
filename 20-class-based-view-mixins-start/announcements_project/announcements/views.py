b62dfaa542cd3804792f1b7e364b537cf5a1ddd5
Daniel Mouris <dgmouris@gmail.com>
2026-03-23 10:35:17 -0600

    class progress pushed

--- 20-class-based-view-mixins-start/announcements_project/announcements/views.py
+++ 20-class-based-view-mixins-start/announcements_project/announcements/views.py
@@ -1,48 +1,49 @@
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
+
 # import login_required decorator
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import (
+    login_required,
+    user_passes_test,
+    permission_required,
+)
 
 # Create your views here.
 from .models import Announcement
 from .forms import AnnouncementForm
 
+# let's import our mixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 # our test function here.
 def is_teacher(user):
     # the user object is passed in here by the decorator
-    return user.role == 'teacher'
+    return user.role == "teacher"
 
 
-@method_decorator(login_required, name='dispatch')
-class AnnouncementListView(View):
-    template_name = 'announcements/announcement_list.html'
+class AnnouncementListView(LoginRequiredMixin, View):
+    template_name = "announcements/announcement_list.html"
 
     def get(self, request):
-        announcements = Announcement.objects.all().order_by('-created_at')
-        return render(
-            request,
-            self.template_name,
-            {'announcements': announcements}
-        )
-
+        announcements = Announcement.objects.all().order_by("-created_at")
+        return render(request, self.template_name, {"announcements": announcements})
 
 
 # this will restrict access to only users that pass the is_teacher test
 # it will redirect to the login page if the user does not have permission.
-from django.views import View
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required, user_passes_test
 
-@method_decorator(login_required, name='dispatch')
-@method_decorator(user_passes_test(is_teacher, login_url='login'), name='dispatch')
+
+@method_decorator(login_required, name="dispatch")
+@method_decorator(user_passes_test(is_teacher, login_url="login"), name="dispatch")
 class CreateAnnouncementView(View):
-    template_name = 'announcements/create_announcement.html'
+    template_name = "announcements/create_announcement.html"
     form_class = AnnouncementForm
 
     def get(self, request, *args, **kwargs):
         form = self.form_class()
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {"form": form})
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
@@ -50,5 +51,5 @@ class CreateAnnouncementView(View):
             announcement = form.save(commit=False)
             announcement.created_by = request.user
             announcement.save()
-            return redirect('announcement_list')
-        return render(request, self.template_name, {'form': form})
+            return redirect("announcement_list")
+        return render(request, self.template_name, {"form": form})
