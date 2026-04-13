27afacec48001ef6174c60891ae94669c0d8fc82
Daniel Mouris <dgmouris@gmail.com>
2026-04-13 10:42:26 -0600

    class progress pushed

--- 23-multi-user-rest-api-start/track_workout_projects/workouts_app/permissions.py
+++ 23-multi-user-rest-api-start/track_workout_projects/workouts_app/permissions.py
@@ -3,12 +3,12 @@
 # because you want to share them across apps.
 
 # we're going to use this permission and override it.
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import BasePermission
 
 
 # in this permission we're going to say if a user is trying to modify a workout log
 # it should only be that user.
-class IsOwnerOfResourceOrReadOnly(IsAuthenticated):
+class IsOwnerOfResourceOrReadOnly(BasePermission):
     """
     Custom permission to allow owners of an object to edit it.
     This assumes that a model has a `user` attribute.
@@ -16,6 +16,7 @@ class IsOwnerOfResourceOrReadOnly(IsAuthenticated):
 
     # this returns a boolean whether they have permission or not.
     def has_object_permission(self, request, view, obj):
+        breakpoint()
         # Read permissions on any request.
         if request.method in ("GET", "HEAD", "OPTIONS"):
             return True
